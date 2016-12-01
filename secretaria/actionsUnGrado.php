7fc510a7bc70e6de69d660b4214fb52cd2a40c90
ignasiballart <ignasi.ballart@gmail.com>
2016-12-01 18:26:15 +0100

    Grados update in progress

--- secretaria/actionsUnGrado.php
+++ secretaria/actionsUnGrado.php
@@ -18,10 +18,6 @@ try
 
 			$recordCount = mysqli_num_rows($result);
 
-			$_GET["jtSorting"] = 2;
-			$_GET["jtStartIndex"]= 1; 
-			$_GET["jtPageSize"] = 2;
-
 			$consulta =	"SELECT gra.id_grado, per.nombre, gal.curso_esc,gal.baixa 
 							FROM persona AS per, alumno AS alu, grados_alumnos AS gal, grados AS gra 
 							WHERE per.id_persona = alu.id_alumno AND alu.id_alumno = gal.id_alumno AND gal.id_grado = gra.id_grado AND gra.id_grado = '{$_GET['gradoid']}'
@@ -45,13 +41,12 @@ try
 
 	}
 	// else if($_GET["action"] == "update"){
-	// 	if (isset($_GET['materia'])) {
-	// 		//comprovar que materia existe
-	// 		//comprovar que el professor la imparte
 
-	// 		$consulta = 'UPDATE matricula, asignatura SET matricula.nota = "'.$_POST['nota'].'" 
-	// 					 WHERE matricula.id_asignatura = asignatura.id_asignatura 
-	// 					 AND asignatura.codigo = "'.$_GET['materia'].'"';
+	// 		$consulta = 'UPDATE persona AS per, alumno AS alu, grados_alumnos AS gal, grados AS gra
+	// 					SET per.nombre = "'.$_POST['nombre'].'", gal.curso_esc = "'.$_POST['curso'].'", 
+	// 					gal.baixa = "'.$_POST['baixa'].'",
+	// 					WHERE per.id_persona = alu.id_alumno AND alu.id_alumno = gal.id_alumno AND gal.id_grado = gra.id_grado 
+	// 					AND gra.id_grado = "'.$_GET['gradoid'].'"';
 
 	// 		$result = mysqli_query($connect, $consulta);
 	// 		//imprimirlos
@@ -59,9 +54,20 @@ try
 	// 		$jTableResult['Result'] = "OK";
 	// 		print json_encode($jTableResult);
 	// 		mysqli_close($connect);
-	// 	}
 	// }
 
+	else if($_GET["action"] == "update"){
+
+			$consulta = 'UPDATE persona
+						SET persona.nombre = "'.$_GET['gradoid'].'"';
+
+			$result = mysqli_query($connect, $consulta);
+			//imprimirlos
+			$jTableResult = array();
+			$jTableResult['Result'] = "OK";
+			print json_encode($jTableResult);
+			mysqli_close($connect);
+	}
 
 }
 catch(Exception $ex)
