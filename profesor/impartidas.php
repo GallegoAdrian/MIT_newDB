e9550bb1024f376c48946e1eee04dd3ae1911530
Adrián Gallego <gallego.adrian@gmail.com>
2016-12-07 16:15:48 +0100

    UNIFICACION STYLES JTABLES

--- profesor/impartidas.php
+++ profesor/impartidas.php
@@ -38,7 +38,7 @@ $asignatura = getAsignatura($_GET['a'], $connect);
 	<!--JTABLES: start-->
 	<script src="../scripts/jquery-1.6.4.min.js"></script>
 	<link href="../themes/redmond/jquery-ui-1.8.16.custom.css" rel="stylesheet" type="text/css" />
-	<link href="../scripts/jtable/themes/lightcolor/blue/jtable.css" rel="stylesheet" type="text/css" />
+	<link href="../scripts/jtable/themes/metro/darkgray/jtable.css" rel="stylesheet" type="text/css" />
     <script src="../scripts/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
     <script src="../scripts/jtable/jquery.jtable.js" type="text/javascript"></script>
 	<!--JTABLES: end-->
